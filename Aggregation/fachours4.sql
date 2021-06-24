1fb48d124ddcfe12bf743f7f3c03a229ac405e83
overridelife <akzholim550@gmail.com>
2021-06-24 14:55:17 +0200

    rank members

--- Aggregation/fachours4.sql
+++ Aggregation/fachours4.sql
@@ -1,4 +1,19 @@
 /*
-Output the facility id that has the highest number of slots booked.
-Ensure that in the event of a tie, all tieing results get output.
-*/
+ Output the facility id that has the highest number of slots booked.
+ Ensure that in the event of a tie, all tieing results get output.
+ */
+WITH base AS (
+    SELECT 
+        facid,
+        SUM(slots) AS total,
+        RANK() OVER (
+            ORDER BY SUM(slots) DESC
+        ) AS r
+    FROM cd.bookings
+    GROUP BY 1
+)
+SELECT 
+    facid,
+    total
+FROM base
+WHERE r = 1;
